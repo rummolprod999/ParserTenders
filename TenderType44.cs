655e08ee7587f48261c98b2b051d20aae4c6cd12
rummolprod999 <rummolprod999@gmail.com>
2017-06-02 11:28:51 +0300

    update

--- TenderType44.cs
+++ TenderType44.cs
@@ -139,7 +139,74 @@ namespace ParserTenders
                     int id_customer = 0;
                     if (!String.IsNullOrEmpty(organizer_reg_num))
                     {
-                        string select_org = $"";
+                        string select_org =
+                            $"SELECT id_organizer FROM {Program.Prefix}organizer WHERE reg_num = @reg_num";
+                        MySqlCommand cmd4 = new MySqlCommand(select_org, connect);
+                        cmd4.Prepare();
+                        cmd4.Parameters.AddWithValue("@reg_num", organizer_reg_num);
+                        MySqlDataReader reader2 = cmd4.ExecuteReader();
+                        if (reader2.HasRows)
+                        {
+                            reader2.Read();
+                            id_organizer = reader2.GetInt32("id_organizer");
+                            reader2.Close();
+                        }
+                        else
+                        {
+                            reader2.Close();
+                            string add_organizer =
+                                $"INSERT INTO {Program.Prefix}organizer SET reg_num = @reg_num, full_name = @full_name, post_address = @post_address, fact_address = @fact_address, inn = @inn, kpp = @kpp, responsible_role = @responsible_role, contact_person = @contact_person, contact_email = @contact_email, contact_phone = @contact_phone, contact_fax = @contact_fax";
+                            MySqlCommand cmd5 = new MySqlCommand(add_organizer, connect);
+                            cmd5.Prepare();
+                            cmd5.Parameters.AddWithValue("@reg_num", organizer_reg_num);
+                            cmd5.Parameters.AddWithValue("@full_name", organizer_full_name);
+                            cmd5.Parameters.AddWithValue("@post_address", organizer_post_address);
+                            cmd5.Parameters.AddWithValue("@fact_address", organizer_fact_address);
+                            cmd5.Parameters.AddWithValue("@inn", organizer_inn);
+                            cmd5.Parameters.AddWithValue("@kpp", organizer_kpp);
+                            cmd5.Parameters.AddWithValue("@responsible_role", organizer_responsible_role);
+                            cmd5.Parameters.AddWithValue("@contact_person", organizer_contact);
+                            cmd5.Parameters.AddWithValue("@contact_email", organizer_email);
+                            cmd5.Parameters.AddWithValue("@contact_phone", organizer_phone);
+                            cmd5.Parameters.AddWithValue("@contact_fax", organizer_fax);
+                            cmd5.ExecuteNonQuery();
+                            id_customer = (int) cmd5.LastInsertedId;
+                        }
+                    }
+                    else
+                    {
+                        Log.Logger("Нет organizer_reg_num", file_path);
+                    }
+
+                    int id_placing_way = 0;
+                    string placingWay_code = ((string) tender.SelectToken("placingWay.code") ?? "").Trim();
+                    string placingWay_name = ((string) tender.SelectToken("placingWay.name") ?? "").Trim();
+                    if (!String.IsNullOrEmpty(placingWay_code))
+                    {
+                        string select_placing_way =
+                            $"SELECT id_placing_way FROM {Program.Prefix}placing_way WHERE code = @code";
+                        MySqlCommand cmd6 = new MySqlCommand(select_placing_way, connect);
+                        cmd6.Prepare();
+                        cmd6.Parameters.AddWithValue("@code", placingWay_code);
+                        MySqlDataReader reader3 = cmd6.ExecuteReader();
+                        if (reader3.HasRows)
+                        {
+                            reader3.Read();
+                            id_placing_way = reader3.GetInt32("id_placing_way");
+                            reader3.Close();
+                        }
+                        else
+                        {
+                            reader3.Close();
+                            string insert_placing_way =
+                                $"INSERT INTO {Program.Prefix}placing_way SET code= @code, name= @name";
+                            MySqlCommand cmd7 = new MySqlCommand(insert_placing_way, connect);
+                            cmd7.Prepare();
+                            cmd7.Parameters.AddWithValue("@code", placingWay_code);
+                            cmd7.Parameters.AddWithValue("@name", placingWay_name);
+                            cmd7.ExecuteNonQuery();
+                            id_placing_way = (int) cmd7.LastInsertedId;
+                        }
                     }
                 }
             }
