1e449ad86ecd1248fb971ecd3dfca423aaf472f6
rummolprod999 <rummolprod999@gmail.com>
2019-03-26 14:39:29 +0300

    add type sign project 44

--- ParserDir/ParserSignProj44.cs
+++ ParserDir/ParserSignProj44.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 
 namespace ParserTenders.ParserDir
 {
@@ -14,6 +17,47 @@ namespace ParserTenders.ParserDir
 
         public override void Parsing()
         {
+            DtRegion = GetRegions();
+            foreach (DataRow row in DtRegion.Rows)
+            {
+                var arch = new List<string>();
+                var pathParse = "";
+                var regionPath = (string) row["path"];
+                switch (Program.Periodparsing)
+                {
+                    case TypeArguments.Last44:
+                        pathParse = $"/fcs_regions/{regionPath}/contractprojects/";
+                        arch = GetListArchLast(pathParse, regionPath);
+                        break;
+                    case TypeArguments.Curr44:
+                        pathParse = $"/fcs_regions/{regionPath}/contractprojects/currMonth/";
+                        arch = GetListArchCurr(pathParse, regionPath);
+                        break;
+                    case TypeArguments.Prev44:
+                        pathParse = $"/fcs_regions/{regionPath}/contractprojects/prevMonth/";
+                        arch = GetListArchPrev(pathParse, regionPath);
+                        break;
+                }
+
+                if (arch.Count == 0)
+                {
+                    Log.Logger("Получен пустой список архивов", pathParse);
+                    continue;
+                }
+
+                foreach (var v in arch)
+                {
+                    GetListFileArch(v, pathParse, (string) row["conf"], (int) row["id"]);
+                }
+            }
+        }
+
+        public override List<String> GetListArchLast(string pathParse, string regionPath)
+        {
+            var archtemp = GetListFtp44(pathParse);
+            var yearsSearch = Program.Years.Select(y => $"contractproject_{regionPath}{y}").ToList();
+            yearsSearch.AddRange(Program.Years.Select(y => $"contractproject{y}").ToList());
+            return archtemp.Where(a => yearsSearch.Any(t => a.IndexOf(t, StringComparison.Ordinal) != -1)).ToList();
         }
     }
 }
