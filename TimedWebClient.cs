18f9547dbfb92bb9d4253c6a6f4af5183f3bbd08
rummolprod999 <rummolprod999@gmail.com>
2020-01-22 11:24:20 +0300

    add web parser 223

--- TimedWebClient.cs
+++ TimedWebClient.cs
@@ -8,7 +8,7 @@ namespace ParserTenders
         protected override WebRequest GetWebRequest(Uri address)
         {
             WebRequest wr = base.GetWebRequest(address);
-            wr.Timeout = 600000;
+            wr.Timeout = 300000;
             return wr;
         }
     }
@@ -18,7 +18,7 @@ namespace ParserTenders
         protected override WebRequest GetWebRequest(Uri address)
         {
             HttpWebRequest wr = (HttpWebRequest)base.GetWebRequest(address);
-            wr.Timeout = 600000;
+            wr.Timeout = 60000;
             wr.UserAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0";
             /*wr.Headers[HttpRequestHeader.UserAgent] =
                 "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0";*/
